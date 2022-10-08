98a0967397b70ee910dff8387aab641456591fb4
AntoninaVi <tonatonotonya@gmail.com>
2022-10-08 22:27:56 +0300

    modal?

--- js/index.js
+++ js/index.js
@@ -12,5 +12,26 @@ const gra = function (min, max) {
 // init();
 
 
+// const elPromo = document.querySelector('main__page-swiper-item');
+// const elCounter = document.getElementById('counter');
 
+// const promoTop = () => {
+//   const pixels = elPromo.getBoundingClientRect().top;
+//   elCounter.textContent = `${Math.floor(pixels / 20)} `;
+// }
+ 
+// const onScroll = (e) => {
+//   promoTop();
+// }
 
+// document.addEventListener('scroll', onScroll);
+// onScroll();
+
+import * as basicLightbox from 'basiclightbox'
+document.querySelector('button.iframe').onclick = () => {
+
+	basicLightbox.create(`
+		<iframe width="560" height="315" src="https://www.youtube.com/watch?v=geFi-ZpN2ZM" frameborder="0" allowfullscreen></iframe>
+	`).show()
+
+}
