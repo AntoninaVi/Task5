c43ee484d3ba12d182bc5c28f3a9a07f0625e77a
AntoninaVi <tonatonotonya@gmail.com>
2022-10-24 16:24:12 +0300

    modal window

--- js/index.js
+++ js/index.js
@@ -20,3 +20,59 @@ window.onclick = function (event) {
     modal.style.display = "none"
   }
 }
+
+let modal2 = document.getElementById('myModal2');
+let btn2 = document.getElementById('myBtn2');
+let span2 = document.getElementsByClassName("close2")[0];
+
+btn2.onclick = function () {
+  modal2.style.display = "block";
+}
+span2.onclick = function () {
+  modal2.style.display = "none";
+}
+
+window.onclick = function (event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none"
+  }
+}
+
+
+
+let modal3 = document.getElementById('myModal3');
+let btn3 = document.getElementById('myBtn3');
+let span3 = document.getElementsByClassName("close3")[0];
+
+btn3.onclick = function () {
+  modal3.style.display = "block";
+}
+span3.onclick = function () {
+  modal3.style.display = "none";
+}
+
+window.onclick = function (event) {
+  if (event.target == modal3) {
+    modal3.style.display = "none"
+  }
+}
+
+let modal4 = document.getElementById('myModal4');
+let btn4 = document.getElementById('myBtn4');
+let span4 = document.getElementsByClassName("close4")[0];
+
+btn4.onclick = function () {
+  modal4.style.display = "block";
+}
+span4.onclick = function () {
+  modal4.style.display = "none";
+}
+
+window.onclick = function (event) {
+  if (event.target == modal4) {
+    modal4.style.display = "none"
+  }
+}
+
+
+
